ee48a1d1cf2975373a511cf4b688a39770d4112f
Julien Bernard <julien.bernard@univ-fcomte.fr>
2016-08-22 23:31:24 +0200

    change Direction and add Orientation

--- include/gf/Direction.h
+++ include/gf/Direction.h
@@ -31,111 +31,92 @@ inline namespace v1 {
 
   /**
    * @ingroup core
-   * @brief Cardinal and ordinal directions
+   * @brief Main four directions
    *
-   * gf::Direction represents one the four cardinal directions and the four
-   * ordinal (or intercardinal) directions. A special value is added to
-   * represent the center, it indicates no direction.
-   *
+   * gf::Direction represents one of the four main directions. A special value
+   * is added to represent the center, it indicates no direction.
    */
-  class GF_API Direction {
-  public:
-    /**
-     * @brief Constructor
-     *
-     * By default, no direction, i.e. center (see gf::Direction::Center)
-     */
-    Direction()
-    : m_dir(-1)
-    {
-    }
-
-    /**
-     * @brief Get a unit vector from a direction
-     *
-     * @returns A unit vector representing the direction
-     */
-    Vector2f getUnit() const;
-
-    /**
-     * @brief Get a vector from a direction
-     *
-     * The vector has its coordinates at -1, 0 or 1 depending on the direction.
-     * It can be used to represent the displacement on a grid in the given
-     * direction.
-     *
-     * @returns A vector representing the direction
-     */
-    Vector2i getVector() const;
-
-    /**
-     * @brief Get an angle from a direction
-     *
-     * North is at angle 0 and angle grows clockwise.
-     *
-     * @returns an angle (in radians) representing the direction
-     */
-    float getAngle() const;
-
-    /**
-     * @brief Get the opposite direction
-     *
-     * @return The opposite direction
-     */
-    Direction getOppositeDirection() const;
-
-    /**
-     * @brief Get the orthogonal direction clockwise
-     *
-     * @return The orthogonal direction clockwise
-     */
-    Direction getOrthogonalDirectionCW() const;
-
-    /**
-     * @brief Get the orthogonal direction counter-clockwise
-     *
-     * @return The orthogonal direction counter-clockwise
-     */
-    Direction getOrthogonalDirectionCCW() const;
-
-    /**
-     * @brief Get the next direction clockwise
-     *
-     * @return The next direction clockwise
-     */
-    Direction getNextDirectionCW() const;
-
-    /**
-     * @brief Get the next direction counter-clockwise
-     *
-     * @return The next direction counter-clockwise
-     */
-    Direction getNextDirectionCCW() const;
-
-    static const Direction Center;    ///< The center, indicates no direction
-    static const Direction North;     ///< The north direction
-    static const Direction NorthEast; ///< The north-east direction
-    static const Direction East;      ///< The east direction
-    static const Direction SouthEast; ///< The south-east direction
-    static const Direction South;     ///< The south direction
-    static const Direction SouthWest; ///< The south-west direction
-    static const Direction West;      ///< The west direction
-    static const Direction NorthWest; ///< The north-west direction
-
-  private:
-    constexpr Direction(int dir)
-    : m_dir(dir)
-    {
-
-    }
-
-    int m_dir;
+  enum class Direction : int {
+    Center = -1,
+    Up = 0,
+    Right = 1,
+    Down = 2,
+    Left = 3,
   };
+
+  /**
+   * @brief Get a unit vector from a direction
+   *
+   * @param direction The direction
+   * @returns A unit vector representing the direction
+   */
+  GF_API Vector2f unit(Direction direction);
+
+  /**
+   * @brief Get a vector from a direction
+   *
+   * The vector has its coordinates at -1, 0 or 1 depending on the direction.
+   * It can be used to represent the displacement on a grid in the given
+   * direction.
+   *
+   * @param direction The direction
+   * @returns A vector representing the direction
+   */
+  GF_API Vector2i displacement(Direction direction);
+
+  /**
+   * @brief Get an angle from a direction
+   *
+   * Up is at angle 0 and angle grows clockwise.
+   *
+   * @param direction The direction
+   * @returns an angle (in radians) representing the direction
+   */
+  GF_API float angle(Direction direction);
+
+  /**
+   * @brief Get the opposite direction
+   *
+   * @param direction The direction
+   * @return The opposite direction
+   */
+  GF_API Direction opposite(Direction direction);
+
+  /**
+   * @brief Get the orthogonal direction clockwise
+   *
+   * @param direction The direction
+   * @return The orthogonal direction clockwise
+   */
+  GF_API Direction orthogonalCW(Direction direction);
+
+  /**
+   * @brief Get the orthogonal direction counter-clockwise
+   *
+   * @param direction The direction
+   * @return The orthogonal direction counter-clockwise
+   */
+  GF_API Direction orthogonalCCW(Direction direction);
+
+  /**
+   * @brief Get the next direction clockwise
+   *
+   * @param direction The direction
+   * @return The next direction clockwise
+   */
+  GF_API Direction nextCW(Direction direction);
+
+  /**
+   * @brief Get the next direction counter-clockwise
+   *
+   * @param direction The direction
+   * @return The next direction counter-clockwise
+   */
+  GF_API Direction nextCCW(Direction direction);
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 }
 #endif
 }
-
 
 #endif // GF_DIRECTION_H
---
+++ include/gf/Orientation.h
@@ -0,0 +1,131 @@
+/*
+ * Gamedev Framework (gf)
+ * Copyright (C) 2016 Julien Bernard
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+#ifndef GF_DIRECTION_H
+#define GF_DIRECTION_H
+
+#include "Portability.h"
+#include "Vector.h"
+
+namespace gf {
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+inline namespace v1 {
+#endif
+
+  /**
+   * @ingroup core
+   * @brief Cardinal and ordinal orientation
+   *
+   * gf::Orientation represents one the four cardinal orientations and the
+   * four ordinal (or intercardinal) orientations. A special value is added to
+   * represent the center, it indicates no orientation.
+   *
+   * @sa gf::Direction
+   */
+  enum class Orientation : int {
+    Center    = -1, ///< The center, indicates no orientation
+    North     =  0, ///< The north orientation
+    NorthEast =  1, ///< The north-east orientation
+    East      =  2, ///< The east orientation
+    SouthEast =  3, ///< The south-east orientation
+    South     =  4, ///< The south orientation
+    SouthWest =  5, ///< The south-west orientation
+    West      =  6, ///< The west orientation
+    NorthWest =  7, ///< The north-west orientation
+  };
+
+
+  /**
+   * @brief Get a unit vector from an orientation
+   *
+   * @param orientation The orientation
+   * @returns A unit vector representing the orientation
+   */
+  GF_API Vector2f unit(Orientation orientation);
+
+  /**
+   * @brief Get a vector from an orientation
+   *
+   * The vector has its coordinates at -1, 0 or 1 depending on the orientation.
+   * It can be used to represent the displacement on a grid in the given
+   * orientation.
+   *
+   * @param orientation The orientation
+   * @returns A vector representing the orientation
+   */
+  GF_API Vector2i displacement(Orientation orientation);
+
+  /**
+   * @brief Get an angle from a orientation
+   *
+   * North is at angle 0 and angle grows clockwise.
+   *
+   * @param orientation The orientation
+   * @returns an angle (in radians) representing the orientation
+   */
+  GF_API float angle(Orientation orientation);
+
+  /**
+   * @brief Get the opposite orientation
+   *
+   * @param orientation The orientation
+   * @return The opposite orientation
+   */
+  GF_API Orientation opposite(Orientation orientation);
+
+  /**
+   * @brief Get the orthogonal orientation clockwise
+   *
+   * @param orientation The orientation
+   * @return The orthogonal orientation clockwise
+   */
+  GF_API Orientation orthogonalCW(Orientation orientation);
+
+  /**
+   * @brief Get the orthogonal orientation counter-clockwise
+   *
+   * @param orientation The orientation
+   * @return The orthogonal orientation counter-clockwise
+   */
+  GF_API Orientation orthogonalCCW(Orientation orientation);
+
+  /**
+   * @brief Get the next orientation clockwise
+   *
+   * @param orientation The orientation
+   * @return The next orientation clockwise
+   */
+  GF_API Orientation nextCW(Orientation orientation);
+
+  /**
+   * @brief Get the next orientation counter-clockwise
+   *
+   * @param orientation The orientation
+   * @return The next orientation counter-clockwise
+   */
+  GF_API Orientation nextCCW(Orientation orientation);
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+}
+#endif
+}
+
+
+#endif // GF_DIRECTION_H
