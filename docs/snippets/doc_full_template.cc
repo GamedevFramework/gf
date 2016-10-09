077fa38befc4fe2a09ae793dc8149d23743ab12b
Julien Bernard <julien.bernard@univ-fcomte.fr>
2016-10-09 23:32:10 +0200

    add a template for starting a game with gf

---
+++ docs/snippets/doc_full_template.cc
@@ -0,0 +1,164 @@
+/*
+ * Gamedev Framework (gf)
+ * Copyright (C) 2016 Julien Bernard
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+/// [full]
+#include <gf/Action.h>
+#include <gf/Clock.h>
+#include <gf/Color.h>
+#include <gf/EntityContainer.h>
+#include <gf/Event.h>
+#include <gf/RenderWindow.h>
+#include <gf/ViewContainer.h>
+#include <gf/Views.h>
+#include <gf/Window.h>
+
+
+int main() {
+  static constexpr gf::Vector2u ScreenSize(1024, 576);
+  static constexpr gf::Vector2f ViewSize(100.0f, 100.0f); // dummy values
+
+  // initialization
+
+  gf::Window window("Game", ScreenSize);
+  gf::RenderWindow renderer(window);
+
+  // views
+
+  gf::ViewContainer views;
+
+  gf::ExtendView mainView;
+  mainView.setSize(ViewSize);
+  views.addView(mainView);
+
+  gf::ScreenView hudView;
+  views.addView(hudView);
+
+  views.onScreenResize(ScreenSize);
+
+  // actions
+
+  gf::ActionContainer actions;
+
+  gf::Action closeWindowAction("Close window");
+  closeWindowAction.addCloseControl();
+  closeWindowAction.addKeycodeKeyControl(gf::Keycode::Escape);
+  actions.addAction(closeWindowAction);
+
+  gf::Action fullscreenAction("Fullscreen");
+  fullscreenAction.addKeycodeKeyControl(gf::Keycode::F);
+  actions.addAction(fullscreenAction);
+
+  gf::Action leftAction("Left");
+  leftAction.addScancodeKeyControl(gf::Scancode::A);
+  leftAction.addScancodeKeyControl(gf::Scancode::Left);
+  leftAction.setContinuous();
+  actions.addAction(leftAction);
+
+  gf::Action rightAction("Right");
+  rightAction.addScancodeKeyControl(gf::Scancode::D);
+  rightAction.addScancodeKeyControl(gf::Scancode::Right);
+  rightAction.setContinuous();
+  actions.addAction(rightAction);
+
+  gf::Action upAction("Up");
+  upAction.addScancodeKeyControl(gf::Scancode::W);
+  upAction.addScancodeKeyControl(gf::Scancode::Up);
+  upAction.setContinuous();
+  actions.addAction(upAction);
+
+  gf::Action downAction("Down");
+  downAction.addScancodeKeyControl(gf::Scancode::S);
+  downAction.addScancodeKeyControl(gf::Scancode::Down);
+  downAction.setContinuous();
+  actions.addAction(downAction);
+
+  // entities
+
+  gf::EntityContainer mainEntities;
+  // add entities to mainEntities
+
+  gf::EntityContainer hudEntities;
+  // add entities to hudEntities
+
+  // game loop
+
+  renderer.clear(gf::Color::White);
+
+  gf::Clock clock;
+  bool fullscreen = false;
+
+  while (window.isOpen()) {
+    // 1. input
+
+    gf::Event event;
+
+    while (window.pollEvent(event)) {
+      actions.update(event);
+      views.update(event);
+    }
+
+    if (closeWindowAction.isActive()) {
+      window.close();
+    }
+
+    if (fullscreenAction.isActive()) {
+      fullscreen = !fullscreen;
+      window.setFullscreen(fullscreen);
+    }
+
+    if (rightAction.isActive()) {
+      // do something
+    } else if (leftAction.isActive()) {
+      // do something
+    } else if (upAction.isActive()) {
+      // do something
+    } else if (downAction.isActive()) {
+      // do something
+    } else {
+      // do something
+    }
+
+
+    // 2. update
+
+    float dt = clock.restart().asSeconds();
+    mainEntities.update(dt);
+    hudEntities.update(dt);
+
+
+    // 3. draw
+
+    renderer.clear();
+
+    renderer.setView(mainView);
+    mainEntities.render(renderer);
+
+    renderer.setView(hudView);
+    hudEntities.render(renderer);
+
+    renderer.display();
+
+    actions.reset();
+  }
+
+  return 0;
+}
+/// [full]
