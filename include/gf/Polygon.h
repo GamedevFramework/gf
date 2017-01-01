adee62ab33b653f3e92b654c8a60e4b2e9a22bc6
Julien Bernard <julien.bernard@univ-fcomte.fr>
2017-01-01 18:10:29 +0100

    add documentation to Polygon

--- include/gf/Polygon.h
+++ include/gf/Polygon.h
@@ -31,10 +31,23 @@ namespace gf {
 inline namespace v1 {
 #endif
 
+  /**
+   * @brief A convex polygon
+   *
+   */
   class GF_API Polygon {
   public:
+    /**
+     * @brief Default constructor
+     */
     Polygon() = default;
 
+    /**
+     * @brief Constructor from points
+     *
+     * @param first Iterator to the first point
+     * @param last Iterator after the last point
+     */
     template<typename Iterator>
     Polygon(Iterator first, Iterator last)
     : m_points(first, last)
@@ -42,13 +55,42 @@ inline namespace v1 {
 
     }
 
+    /**
+     * @brief Add a point to the polygon
+     *
+     * @param point The point to add to the polygon
+     */
     void addPoint(Vector2f point);
 
+    /**
+     * @brief Get the number of points of the polygon
+     *
+     * @returns The number of points of the polygon
+     */
     std::size_t getPointCount() const;
+
+    /**
+     * @brief Get the i-th point of the polygon
+     *
+     * @param index The index of the point
+     */
     Vector2f getPoint(std::size_t index) const;
 
+    /**
+     * @brief Get the center of the polygon
+     *
+     * As the polygon is convex, the center is inside the polygon
+     *
+     * @returns The center of the polygon
+     */
     Vector2f getCenter() const;
 
+    /**
+     * @brief Get the farthest point in a direction
+     *
+     * @param direction The direction to search
+     * @returns The farthest point of the polygon in the given direction
+     */
     Vector2f getSupport(Vector2f direction) const;
 
   private:
