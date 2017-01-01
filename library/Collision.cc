c62a09a8c5ca22225cce028a6f8475166ddbf592
Julien Bernard <julien.bernard@univ-fcomte.fr>
2017-01-01 22:59:33 +0100

    add first raw implementation of GJK and EPA

--- library/Collision.cc
+++ library/Collision.cc
@@ -20,6 +20,11 @@
  */
 #include <gf/Collision.h>
 
+#include <cassert>
+
+#include <queue>
+
+
 namespace gf {
 inline namespace v1 {
 
@@ -135,6 +140,262 @@ inline namespace v1 {
 
     return m.penetration > Skin;
   }
+
+  /*
+   * Polygon-polygon collision
+   * Implementation of the GJK and EPA algorithms
+   */
+
+  static constexpr float Epsilon = std::numeric_limits<float>::epsilon();
+
+  // types (in an anonymous namespace)
+
+  namespace {
+    class Simplex {
+    public:
+      Simplex()
+      : m_size(0)
+      {
+
+      }
+
+      unsigned getSize() const {
+        return m_size;
+      }
+
+      void add(Vector2f a) {
+        assert(m_size < 3);
+        m_v[m_size] = a;
+        m_size++;
+      }
+
+      void remove(unsigned index) {
+        assert(index < 3);
+
+        switch (index) {
+          case 0:
+            m_v[0] = m_v[1];
+          case 1:
+            m_v[1] = m_v[2];
+          default:
+            break;
+        }
+
+        m_size--;
+      }
+
+      Vector2f getLast() const {
+        assert(m_size > 0);
+        return m_v[m_size - 1];
+      }
+
+      Vector2f operator[](unsigned index) const {
+        assert(index < 3);
+        return m_v[index];
+      }
+
+    private:
+      unsigned m_size;
+      Vector2f m_v[3];
+    };
+
+    struct Edge {
+      Vector2f p1;
+      Vector2f p2;
+      Vector2f normal;
+      float distance;
+    };
+
+    struct EdgeCompare { // for priority_queue
+      bool operator()(const Edge& lhs, const Edge& rhs) {
+        return lhs.distance > rhs.distance;
+      }
+    };
+
+    enum class Winding {
+      Clockwise,
+      Counterclockwise,
+    };
+
+  }
+
+  static Edge getEdge(Vector2f p1, Vector2f p2, Winding winding) {
+    Edge ret;
+    ret.normal = p2 - p1;
+
+    if (winding == Winding::Clockwise) {
+      ret.normal = -gf::perp(ret.normal);
+    } else {
+      ret.normal = gf::perp(ret.normal);
+    }
+
+    ret.normal = gf::normalize(ret.normal);
+
+    ret.distance = std::abs(gf::dot(p1, ret.normal));
+    ret.p1 = p1;
+    ret.p2 = p2;
+
+    return ret;
+  }
+
+  namespace {
+
+    class ExpandingSimplex {
+    public:
+      ExpandingSimplex(const Simplex& simplex) {
+        m_winding = getWinding(simplex);
+
+        unsigned size = simplex.getSize();
+
+        for (unsigned i = 0; i < size; ++i) {
+          unsigned j = (i + 1) % size;
+          m_queue.push(getEdge(simplex[i], simplex[j], m_winding));
+        }
+      }
+
+      Edge getClosestEdge() const {
+        return m_queue.top();
+      }
+
+      void expand(Vector2f point) {
+        Edge edge = m_queue.top();
+        m_queue.pop();
+
+        m_queue.push(getEdge(edge.p1, point, m_winding));
+        m_queue.push(getEdge(point, edge.p2, m_winding));
+      }
+
+    private:
+      static Winding getWinding(const Simplex& simplex) {
+        unsigned size = simplex.getSize();
+
+        for (unsigned i = 0; i < size; ++i) {
+          unsigned j = (i + 1) % size;
+          float x = gf::cross(simplex[i], simplex[j]);
+
+          if (x > 0) {
+            return Winding::Clockwise;
+          } else if (x < 0) {
+            return Winding::Counterclockwise;
+          }
+        }
+
+        assert(false);
+        return Winding::Clockwise;
+      }
+
+    private:
+      std::priority_queue<Edge, std::vector<Edge>, EdgeCompare> m_queue;
+      Winding m_winding;
+    };
+
+  }
+
+
+  static Vector2f getSupport(const Polygon& lhs, const Polygon& rhs, Vector2f direction) {
+    return lhs.getSupport(direction) - rhs.getSupport(-direction);
+  }
+
+  static bool checkSimplex(Simplex& simplex, Vector2f& direction) {
+    Vector2f a = simplex.getLast();
+    Vector2f ao = -a;
+
+    if (simplex.getSize() == 3) {
+      Vector2f b = simplex[1];
+      Vector2f c = simplex[0];
+
+      Vector2f ab = b - a;
+      Vector2f ac = c - a;
+
+      Vector2f abPerp = inverseVectorTripleProduct(ac, ab, ab);
+      Vector2f acPerp = inverseVectorTripleProduct(ab, ac, ac);
+
+      if (gf::dot(acPerp, ao) >= 0) {
+        simplex.remove(1);
+        direction = acPerp;
+      } else {
+        if (gf::dot(abPerp, ao) < 0) {
+          return true;
+        }
+
+        simplex.remove(0);
+        direction = abPerp;
+      }
+    } else {
+      Vector2f b = simplex[1];
+      Vector2f ab = b - a;
+
+      direction = inverseVectorTripleProduct(ab, ao, ab);
+
+      if (gf::squareLength(direction) < Epsilon) {
+        direction = gf::perp(ab);
+      }
+    }
+
+    return false;
+  }
+
+
+  static constexpr unsigned MaxIterations = 100;
+
+  bool collides(const Polygon& lhs, const Polygon& rhs, Manifold& m) {
+    /*
+     * Gilbert-Johnson-Keerthi (GJK) algorithm
+     * adapted from http://www.dyn4j.org/2010/04/gjk-gilbert-johnson-keerthi/
+     */
+
+    Vector2f direction = rhs.getCenter() - lhs.getCenter();
+
+    Simplex simplex;
+    simplex.add(getSupport(lhs, rhs, direction));
+
+    if (gf::dot(simplex.getLast(), direction) <= 0.0f) {
+      return false;
+    }
+
+    direction = -direction;
+
+    for (;;) {
+      simplex.add(getSupport(lhs, rhs, direction));
+
+      if (gf::dot(simplex.getLast(), direction) <= 0.0f) {
+        return false;
+      }
+
+      if (checkSimplex(simplex, direction)) {
+        // there is a collision, we do not return yet
+        break;
+      }
+    }
+
+    /*
+     * Expanding Polytope Algorithm (EPA)
+     * adapted from http://www.dyn4j.org/2010/05/epa-expanding-polytope-algorithm/
+     */
+
+    ExpandingSimplex expandingSimplex(simplex);
+    Edge edge;
+    Vector2f point;
+
+    for (unsigned i = 0; i < MaxIterations; ++i) {
+      edge = expandingSimplex.getClosestEdge();
+      point = getSupport(lhs, rhs, edge.normal);
+      float distance = gf::dot(point, edge.normal);
+
+      if (distance - edge.distance < Skin) {
+        m.normal = edge.normal;
+        m.penetration = distance;
+        return true;
+      }
+
+      expandingSimplex.expand(point);
+    }
+
+    m.normal = edge.normal;
+    m.penetration = gf::dot(point, edge.normal);
+    return true;
+  }
+
 
 }
 }
