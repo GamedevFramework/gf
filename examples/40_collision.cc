f98af0cbe255ad99a3bcbc9c64d06bb1a258ae40
Julien Bernard <julien.bernard@univ-fcomte.fr>
2017-01-02 11:14:59 +0100

    add missing header

--- examples/40_collision.cc
+++ examples/40_collision.cc
@@ -18,8 +18,7 @@
  *    misrepresented as being the original software.
  * 3. This notice may not be removed or altered from any source distribution.
  */
-#include <cstdio>
-#include <cmath>
+#include <iostream>
 
 #include <gf/Clock.h>
 #include <gf/Collision.h>
