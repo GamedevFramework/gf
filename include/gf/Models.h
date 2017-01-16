74428d7ae7bcb00f379e50a54279dc1f1c5a1ede
Julien Bernard <julien.bernard@univ-fcomte.fr>
2017-01-16 15:17:25 +0100

    add FixedTimestepModel

---
+++ include/gf/Models.h
@@ -0,0 +1,50 @@
+/*
+ * Gamedev Framework (gf)
+ * Copyright (C) 2016 Julien Bernard
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+#ifndef GF_MODELS_H
+#define GF_MODELS_H
+
+#include "Model.h"
+#include "Portability.h"
+
+namespace gf {
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+inline namespace v1 {
+#endif
+
+  class GF_API FixedTimestepModel : public Model {
+  public:
+    FixedTimestepModel(Model& model, float timestep);
+
+    virtual void update(float dt) override;
+
+  private:
+    Model& m_model;
+    float m_timestep;
+    float m_elapsed;
+  };
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+}
+#endif
+}
+
+
+#endif // GF_MODELS_H
