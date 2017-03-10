6f257aaf066171c879c349120216bc3e4de86325
Julien Bernard <julien.bernard@univ-fcomte.fr>
2017-03-10 16:37:35 +0100

    add support for unity build (off by default)

---
+++ library/XUnityBuild.cc
@@ -0,0 +1,108 @@
+/*
+ * Gamedev Framework (gf)
+ * Copyright (C) 2016-2017 Julien Bernard
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+/*
+ * This file is meant for single compilation unit (a.k.a. unity build)
+ * See: https://en.wikipedia.org/wiki/Single_Compilation_Unit
+ */
+
+#include "Action.cc"
+#include "AnimatedSprite.cc"
+#include "Animation.cc"
+#include "AssetManager.cc"
+#include "Blend.cc"
+#include "BufferedGeometry.cc"
+#include "Circ.cc"
+#include "Clock.cc"
+#include "Collision.cc"
+#include "Color.cc"
+#include "ColorRamp.cc"
+#include "Control.cc"
+#include "Controls.cc"
+#include "Coordinates.cc"
+#include "Curve.cc"
+#include "Curves.cc"
+#include "Direction.cc"
+#include "Drawable.cc"
+#include "Effects.cc"
+#include "Entity.cc"
+#include "EntityContainer.cc"
+#include "Flags.cc"
+#include "Font.cc"
+#include "Gamepad.cc"
+#include "Image.cc"
+#include "InputStream.cc"
+#include "InputStreams.cc"
+#include "Keyboard.cc"
+#include "Library.cc"
+#include "Log.cc"
+#include "Math.cc"
+#include "Matrix.cc"
+#include "MessageManager.cc"
+#include "Model.cc"
+#include "ModelContainer.cc"
+#include "Models.cc"
+#include "Monitor.cc"
+#include "NinePatch.cc"
+#include "Noise.cc"
+#include "Noises.cc"
+#include "Orientation.cc"
+#include "Particles.cc"
+#include "Paths.cc"
+#include "PhysicsBody.cc"
+#include "PhysicsGeometry.cc"
+#include "PhysicsModel.cc"
+#include "Polygon.cc"
+#include "PostProcessing.cc"
+#include "Random.cc"
+#include "Range.cc"
+#include "Rect.cc"
+#include "RenderPipeline.cc"
+#include "RenderTarget.cc"
+#include "RenderTexture.cc"
+#include "RenderWindow.cc"
+#include "ResourceManager.cc"
+#include "Shader.cc"
+#include "Shape.cc"
+#include "Shapes.cc"
+#include "SpriteBatch.cc"
+#include "Sprite.cc"
+#include "SystemInfo.cc"
+#include "Text.cc"
+#include "TextureAtlas.cc"
+#include "Texture.cc"
+#include "TileLayer.cc"
+#include "Time.cc"
+#include "Transformable.cc"
+#include "Transform.cc"
+#include "UI.cc"
+#include "Vector.cc"
+#include "VertexArray.cc"
+#include "VertexBuffer.cc"
+#include "View.cc"
+#include "ViewContainer.cc"
+#include "Views.cc"
+#include "Window.cc"
+
+#include "priv/Debug.cc"
+
+#include "vendor/tinyxml2/tinyxml2.cpp"
+#include "vendor/glad/src/glad.cc"
