e6d00eda4aad3f165f84238283d3f04409715c1a
Julien Bernard <julien.bernard@univ-fcomte.fr>
2017-06-24 00:42:49 +0200

    NinePath::getTexture() now returns a reference

--- include/gf/NinePatch.h
+++ include/gf/NinePatch.h
@@ -94,16 +94,15 @@ inline namespace v1 {
     /**
      * @brief Get the source texture of the nine-patch
      *
-     * If the nine-patch has no source texture, a `nullptr` pointer is returned.
-     * The returned pointer is const, which means that you can't
+     * The returned reference is const, which means that you can't
      * modify the texture when you retrieve it with this function.
      *
-     * @return Pointer to the nine-patch's texture
+     * @return Reference to the nine-patch's texture
      *
      * @sa setTexture()
      */
-    const Texture *getTexture() const {
-      return m_texture;
+    const Texture& getTexture() const {
+      return *m_texture;
     }
 
     /**
