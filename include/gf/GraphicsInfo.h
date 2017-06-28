1e2a5436cd5c622b2f25b6401f800a23d8b6c9b0
Julien Bernard <julien.bernard@univ-fcomte.fr>
2017-06-28 15:38:10 +0200

    add OpenGL names of the properties

--- include/gf/GraphicsInfo.h
+++ include/gf/GraphicsInfo.h
@@ -36,8 +36,12 @@ inline namespace v1 {
    * @ingroup graphics
    * @brief A class for graphics info
    *
-   * This class only contains static methods.
+   * This class only contains static methods. It provides convenient functions
+   * for OpenGL configuration variables. The name of the variables are given
+   * in the documentation of the functions.
    *
+   * @sa [glGetString() - docs.gl](://docs.gl/es2/glGetString)
+   * @sa [glGetIntegerv() - docs.gl](http://docs.gl/es2/glGet)
    */
   class GF_API GraphicsInfo {
   public:
@@ -48,26 +52,36 @@ inline namespace v1 {
 
     /**
      * @brief Get OpenGL vendor string
+     *
+     * OpenGL name: `GL_VENDOR`
      */
     static std::string getVendor();
 
     /**
      * @brief Get OpenGL renderer string
+     *
+     * OpenGL name: `GL_RENDERER`
      */
     static std::string getRenderer();
 
     /**
      * @brief Get OpenGL version string
+     *
+     * OpenGL name: `GL_VERSION`
      */
     static std::string getVersion();
 
     /**
      * @brief Get OpenGL GLSL version string
+     *
+     * OpenGL name: `GL_SHADING_LANGUAGE_VERSION`
      */
     static std::string getShadingLanguageVersion();
 
     /**
      * @brief Get available OpenGL extensions
+     *
+     * OpenGL name: `GL_EXTENSIONS`
      */
     static std::vector<std::string> getExtensions();
 
@@ -82,75 +96,102 @@ inline namespace v1 {
     /**
      * @brief Get a list of supported compressed texture formats
      *
+     * OpenGL name: `GL_NUM_COMPRESSED_TEXTURE_FORMATS` and `GL_COMPRESSED_TEXTURE_FORMATS`
      */
     static std::vector<Format> getCompressedTextureFormats();
 
     /**
      * @brief Get a list of supported shader binary formats
+     *
+     * OpenGL name: `GL_NUM_SHADER_BINARY_FORMATS` and `GL_SHADER_BINARY_FORMATS`
      */
     static std::vector<Format> getShaderBinaryFormats();
 
     /**
      * @brief Get the maximum supported texture image units
+     *
+     * OpenGL name: `GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS`
      */
     static int getMaxCombinedTextureImageUnits();
 
     /**
      * @brief Get a rough estimate of the largest cube-map texture
+     *
+     * OpenGL name: `GL_MAX_CUBE_MAP_TEXTURE_SIZE`
      */
     static int getMaxCubeMapTextureSize();
 
     /**
      * @brief Get the maximum number of four-element vectors that can be held
      * in uniform variable storage for a fragment shader
+     *
+     * OpenGL name: `GL_MAX_FRAGMENT_UNIFORM_VECTORS`
      */
     static int getMaxFragmentUniformVectors();
 
     /**
      * @brief Get the largest renderbuffer width and height
+     *
+     * OpenGL name: `GL_MAX_RENDERBUFFER_SIZE`
      */
     static int getMaxRenderbufferSize();
 
     /**
      * @brief Get the maximum supported texture image units in a fragment shader
+     *
+     * OpenGL name: `GL_MAX_TEXTURE_IMAGE_UNITS`
      */
     static int getMaxTextureImageUnits();
 
     /**
      * @brief Get a rough estimate of the largest texture
+     *
+     * OpenGL name: `GL_MAX_TEXTURE_SIZE`
      */
     static int getMaxTextureSize();
 
     /**
      * @brief Get the maximum number four-element vectors available for varying
      * variables used by vertex and fragment shaders
+     *
+     * OpenGL name: `GL_MAX_VARYING_VECTORS`
      */
     static int getMaxVaryingVectors();
 
     /**
      * @brief Get the maximum number of 4-component generic vertex attributes
      * accessible to a vertex shader
+     *
+     * OpenGL name: `GL_MAX_VERTEX_ATTRIBS`
      */
     static int getMaxVertexAttribs();
 
     /**
      * @brief Get the maximum supported texture image units in a vertex shader
+     *
+     * OpenGL name: `GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS`
      */
     static int getMaxVertexTextureImageUnits();
 
     /**
      * @brief Get the maximum number of four-element vectors that can be held
      * in uniform variable storage for a vertex shader
+     *
+     * OpenGL name: `GL_MAX_VERTEX_UNIFORM_VECTORS`
      */
     static int getMaxVertexUniformVectors();
 
     /**
      * @brief Get the maximum supported width and height of the viewport
+     *
+     * OpenGL name: `GL_MAX_VIEWPORT_DIMS`
      */
     static Vector2i getMaxViewportDims();
 
     /**
      * @brief Get an estimate of the number of bits of subpixel resolution
+     *
+     * OpenGL name: `GL_SUBPIXEL_BITS`
      */
     static int getSubpixelBits();
 
