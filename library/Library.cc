7eb20a5dc1d3681f7e112d7f33e8bdc148933f4d
Sherjil Ozair <sherjilozair@gmail.com>
2018-05-04 20:25:47 -0400

    compile on macos and make example 04 (almost) work

--- library/Library.cc
+++ library/Library.cc
@@ -51,7 +51,7 @@ inline namespace v1 {
         return;
       }
 
-      SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+      SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
       SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
       SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
     }
