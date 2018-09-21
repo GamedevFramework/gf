82e6086374ca98356f615f5aad687650c3e2f69f
Julien Bernard <julien.bernard@univ-fcomte.fr>
2018-09-21 18:01:12 +0200

    add new stream classes

---
+++ include/gf/Stream.h
@@ -0,0 +1,184 @@
+/*
+ * Gamedev Framework (gf)
+ * Copyright (C) 2016-2018 Julien Bernard
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ * Part of this file comes from SFML, with the same license:
+ * Copyright (C) 2007-2015 Laurent Gomila (laurent@sfml-dev.org)
+ */
+#ifndef GF_STREAM_H
+#define GF_STREAM_H
+
+#include <cstddef>
+
+#include "ArrayRef.h"
+#include "BufferRef.h"
+#include "Portability.h"
+
+namespace gf {
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+inline namespace v1 {
+#endif
+
+  /**
+   * @ingroup core
+   * @brief Abstract class for custom file input streams
+   *
+   * This class allows users to define their own stream input sources from
+   * which Gamedev Framework can load resources.
+   *
+   * Gamedev Framework resource classes like gf::Texture and gf::Shader
+   * provide constructors that read data from conventional sources. However,
+   * if you have data coming from a different source (over a network,
+   * embedded, encrypted, compressed, etc) you can derive your own class
+   * from gf::InputStream and use the constructor that takes a generic
+   * input stream.
+   *
+   * @sa gf::OutputStream
+   */
+  class GF_API InputStream {
+  public:
+    /**
+     * @brief Default constructor
+     */
+    InputStream() = default;
+
+    /**
+     * @brief Deleted copy constructor
+     */
+    InputStream(const InputStream&) = delete;
+
+    /**
+     * @brief Deleted copy assignment
+     */
+    InputStream& operator=(const InputStream&) = delete;
+
+    /**
+     * @brief Destructor
+     */
+    virtual ~InputStream();
+
+    /**
+     * @brief Read data from the stream
+     *
+     * After reading, the stream's reading position must be
+     * advanced by the amount of bytes read.
+     *
+     * @param buffer The buffer where to copy the read data
+     *
+     * @return The number of bytes actually read
+     */
+    virtual std::size_t read(BufferRef<uint8_t> buffer) = 0;
+
+    /**
+     * @brief Read a single byte from the stream
+     *
+     * This is a convinience function to read a single byte instead of a
+     * whole buffer.
+     *
+     * @param byte The byte where to copy the read data
+     *
+     * @return The number of bytes actually read
+     */
+    std::size_t read(uint8_t& byte) {
+      return read(BufferRef<uint8_t>(&byte, 1));
+    }
+
+    /**
+     * @brief Change the current reading position
+     *
+     * @param position The position to seek to, from the beginning
+     */
+    virtual void seek(std::ptrdiff_t position) = 0;
+
+    /**
+     * @brief Change the current reading position
+     *
+     * @param position The position to seek to, from the current position
+     */
+    virtual void skip(std::ptrdiff_t position) = 0;
+
+    /**
+     * @brief Tell if the stream is at the end
+     *
+     * @return A boolean indicating the end of the stream
+     */
+    virtual bool isFinished() = 0;
+
+  };
+
+
+  /**
+   * @ingroup core
+   * @brief Abstract class for custom file output streams
+   *
+   * This class allows users to define their own stream output sinks to
+   * which Gamedev Framework can save resources.
+   *
+   * @sa gf::OutputStream
+   */
+  class GF_API OutputStream {
+  public:
+    /**
+     * @brief Default constructor
+     */
+    OutputStream() = default;
+
+    /**
+     * @brief Deleted copy constructor
+     */
+    OutputStream(const InputStream&) = delete;
+
+    /**
+     * @brief Deleted copy assignment
+     */
+    OutputStream& operator=(const InputStream&) = delete;
+
+    /**
+     * @brief Destructor
+     */
+    virtual ~OutputStream();
+
+    /**
+     * @brief Write data to the stream
+     *
+     * @param buffer The buffer to copy to the stream
+     *
+     * @return The number of bytes actually writtent
+     */
+    virtual std::size_t write(ArrayRef<uint8_t> buffer) = 0;
+
+    /**
+     * @brief Write a single byte to the stream
+     *
+     * @param buffer The byte to copy to the stream
+     *
+     * @return The number of bytes actually writtent
+     */
+    std::size_t write(uint8_t byte) {
+      return write(ArrayRef<uint8_t>(&byte, 1));
+    }
+
+  };
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+}
+#endif
+}
+
+#endif // GF_STREAM_H
---
+++ include/gf/Streams.h
@@ -0,0 +1,233 @@
+/*
+ * Gamedev Framework (gf)
+ * Copyright (C) 2016-2018 Julien Bernard
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ * Part of this file comes from SFML, with the same license:
+ * Copyright (C) 2007-2015 Laurent Gomila (laurent@sfml-dev.org)
+ */
+#ifndef GF_STREAMS_H
+#define GF_STREAMS_H
+
+#include <cstddef>
+#include <cstdio>
+
+#include <zlib.h>
+
+#include "Stream.h"
+#include "Path.h"
+#include "Portability.h"
+
+namespace gf {
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+inline namespace v1 {
+#endif
+
+  /**
+   * @ingroup core
+   * @brief File based input stream
+   */
+  class GF_API FileInputStream : public InputStream {
+  public:
+    /**
+     * @brief Constructor
+     *
+     * The constructor opens the file. If the file is not opened, an exception
+     * is thrown.
+     *
+     * @param path The path to the file
+     */
+    explicit FileInputStream(const Path& path);
+
+    /**
+     * @brief Destructor
+     *
+     * The destructor closes the file.
+     */
+    ~FileInputStream();
+
+    virtual std::size_t read(BufferRef<uint8_t> buffer) override;
+    virtual void seek(std::ptrdiff_t position) override;
+    virtual void skip(std::ptrdiff_t position) override;
+    virtual bool isFinished() override;
+
+  private:
+    std::FILE *m_file;
+  };
+
+
+  /**
+   * @ingroup core
+   * @brief Memory based input stream
+   */
+  class GF_API MemoryInputStream : public InputStream {
+  public:
+    /**
+     * @brief Constructor
+     *
+     * This class does not own the memory. The user must keep the memory
+     * available when the stream is active.
+     *
+     * @param memory The source memory
+     */
+    explicit MemoryInputStream(ArrayRef<uint8_t> memory);
+
+    virtual std::size_t read(BufferRef<uint8_t> buffer) override;
+    virtual void seek(std::ptrdiff_t position) override;
+    virtual void skip(std::ptrdiff_t position) override;
+    virtual bool isFinished() override;
+
+  private:
+    ArrayRef<uint8_t> m_memory;
+    std::size_t m_offset;
+  };
+
+
+  /**
+   * @ingroup core
+   * @brief Compressed input stream
+   */
+  class GF_API CompressedInputStream : public InputStream {
+  public:
+    /**
+     * @brief Constructor
+     *
+     * @param compressed The compressed original stream
+     */
+    explicit CompressedInputStream(InputStream& compressed);
+
+    /**
+     * @brief Destructor
+     */
+    virtual ~CompressedInputStream();
+
+    virtual std::size_t read(BufferRef<uint8_t> buffer) override;
+    virtual void seek(std::ptrdiff_t position) override;
+    virtual void skip(std::ptrdiff_t position) override;
+    virtual bool isFinished() override;
+
+  private:
+    static constexpr uInt BufferSize = 256;
+
+    InputStream *m_compressed;
+    z_stream m_stream;
+    uInt m_start;
+    uInt m_stop;
+    bool m_eof;
+    Bytef m_buffer[BufferSize];
+  };
+
+
+
+  /**
+   * @ingroup core
+   * @brief File based output stream
+   */
+  class GF_API FileOutputStream : public OutputStream {
+  public:
+    /**
+     * @brief Open mode for the file
+     */
+    enum class Mode {
+      Write,  ///< Open the file in write only mode
+      Append, ///< Open the file in append only mode
+    };
+
+    /**
+     * @brief Constructor
+     *
+     * The constructor opens the file. If the file is not opened, an exception
+     * is thrown.
+     *
+     * @param path The path to the file
+     */
+    FileOutputStream(const Path& path, Mode mode = Mode::Write);
+
+    /**
+     * @brief Destructor
+     *
+     * The destructor closes the file.
+     */
+    ~FileOutputStream();
+
+    virtual std::size_t write(ArrayRef<uint8_t> buffer) override;
+
+  private:
+    std::FILE *m_file;
+  };
+
+  /**
+   * @ingroup core
+   * @brief Memory based output stream
+   */
+  class GF_API MemoryOutputStream : public OutputStream {
+  public:
+    /**
+     * @brief Constructor
+     *
+     * This class does not own the memory. The user must keep the memory
+     * available when the stream is active.
+     *
+     * @param buffer The destination buffer
+     */
+    explicit MemoryOutputStream(BufferRef<uint8_t> memory);
+
+    virtual std::size_t write(ArrayRef<uint8_t> buffer) override;
+
+  private:
+    BufferRef<uint8_t> m_memory;
+    std::size_t m_offset;
+  };
+
+
+  /**
+   * @ingroup core
+   * @brief Compressed output stream
+   */
+  class GF_API CompressedOutputStream : public OutputStream {
+  public:
+    /**
+     * @brief Constructor
+     *
+     * @param compressed A stream to store the compressed data
+     */
+    explicit CompressedOutputStream(OutputStream& compressed);
+
+    /**
+     * @brief Destructor
+     */
+    ~CompressedOutputStream();
+
+    virtual std::size_t write(ArrayRef<uint8_t> buffer) override;
+
+  private:
+    static constexpr uInt BufferSize = 256;
+
+    OutputStream *m_compressed;
+    z_stream m_stream;
+    Bytef m_buffer[BufferSize];
+  };
+
+
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+}
+#endif
+}
+
+#endif // GF_STREAMS_H
