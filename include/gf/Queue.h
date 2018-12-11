727098dde434f4e70dba140d14d3d4f8e928eb7f
Julien Bernard <julien.bernard@univ-fcomte.fr>
2018-12-11 19:31:29 +0100

    Revert "add default move ctor/assignment"
    This reverts commit f48bfe65573bada409bbdc3e427e2957bfa74f86.

--- include/gf/Queue.h
+++ include/gf/Queue.h
@@ -43,10 +43,6 @@ inline namespace v1 {
   template<typename T>
   class GF_API Queue {
   public:
-    Queue(Queue&&) = default;
-    Queue& operator=(Queue&&) = default;
-
-
     /**
      * @brief Poll a value from the queue, if possible
      *
