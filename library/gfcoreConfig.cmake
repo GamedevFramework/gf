479ce5053c31db535905efa191c784706037c2f7
Julien Bernard <julien.bernard@univ-fcomte.fr>
2019-11-25 15:05:31 +0100

    fix some build issues

--- library/gfcoreConfig.cmake
+++ library/gfcoreConfig.cmake
@@ -1,6 +1,7 @@
 get_filename_component(GFCORE_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(CMakeFindDependencyMacro)
 
+find_dependency(Threads)
 find_dependency(Boost REQUIRED COMPONENTS filesystem)
 
 if(NOT TARGET gf::gfcore0)
