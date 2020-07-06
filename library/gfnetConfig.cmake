0f91badc9f886bff98a99f66bd4d92e618fe3e42
ahugeat <hugeat.arthur@gmail.com>
2020-07-06 18:46:29 +0200

    Refactor CMake to provide components feature

--- library/gfnetConfig.cmake
+++
@@ -1,8 +0,0 @@
-get_filename_component(GFNET_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(CMakeFindDependencyMacro)
-
-find_dependency(gfcore)
-
-if(NOT TARGET gf::gfnet0)
-    include("${GFNET_CMAKE_DIR}/gfnetTargets.cmake")
-endif()
