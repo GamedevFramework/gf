48f49198a360725b76ecf97dc37891f180add188
Julien Bernard <julien.bernard@univ-fcomte.fr>
2020-08-16 14:13:53 +0200

    add an example for multiple windows

---
+++ examples/34_multiple_windows.cc
@@ -0,0 +1,62 @@
+#include <gf/Color.h>
+#include <gf/Event.h>
+#include <gf/RenderWindow.h>
+#include <gf/Window.h>
+
+int main() {
+  gf::Window window0("window0", {640, 480}, ~gf::WindowHints::Resizable);
+  gf::Window window1("window1", {640, 480}, ~gf::WindowHints::Resizable);
+
+  gf::RenderWindow renderer0(window0);
+  gf::RenderWindow renderer1(window1);
+
+  while (window0.isOpen() || window1.isOpen()) {
+    if(window0.isOpen()) {
+      gf::Event event;
+
+      while (window0.pollEvent(event)) {
+        switch (event.type) {
+          case gf::EventType::Closed:
+            window0.close();
+            break;
+          case gf::EventType::KeyPressed:
+            if (event.key.scancode == gf::Scancode::Escape) {
+              window0.close();
+            }
+            break;
+          default:
+            break;
+        }
+      }
+
+      renderer0.setActive();
+      renderer0.clear(gf::Color::Red);
+      renderer0.display();
+    }
+
+    if(window1.isOpen()) {
+      gf::Event event;
+
+      while (window1.pollEvent(event)) {
+        switch (event.type) {
+          case gf::EventType::Closed:
+            window1.close();
+            break;
+          case gf::EventType::KeyPressed:
+            if (event.key.scancode == gf::Scancode::Escape) {
+              window1.close();
+            }
+            break;
+          default:
+            break;
+        }
+      }
+
+      renderer1.setActive();
+      renderer1.clear(gf::Color::Blue);
+      renderer1.display();
+    }
+  }
+
+  return 0;
+}
