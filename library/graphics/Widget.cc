5b1b30e213ff74ee156cbc6890a3e62ee476690c
Julien Bernard <julien.bernard@univ-fcomte.fr>
2021-01-01 00:54:45 +0100

    update copyright year

--- library/graphics/Widget.cc
+++ library/graphics/Widget.cc
@@ -1,6 +1,6 @@
 /*
  * Gamedev Framework (gf)
- * Copyright (C) 2016-2019 Lilian Franchi
+ * Copyright (C) 2016-2021 Lilian Franchi
  *
  * This software is provided 'as-is', without any express or implied
  * warranty.  In no event will the authors be held liable for any damages
