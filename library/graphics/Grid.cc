649dc308a62ceeb5b8070d01bc4d12ebe817bf44
NiiRoZz <extazziii1000@gmail.com>
2021-02-14 22:42:09 +0100

    Fix SquareGrid not rendered correctly

--- library/graphics/Grid.cc
+++ library/graphics/Grid.cc
@@ -87,7 +87,7 @@ inline namespace v1 {
     Vertex vertices[2];
     vertices[0].color = vertices[1].color = m_color;
 
-    for (int i = 0; i < m_gridSize.width; ++i) {
+    for (int i = 0; i <= m_gridSize.width; ++i) {
       float x = i * m_cellSize.width;
       vertices[0].position = { x, 0.0f };
       vertices[1].position = { x, max.y };
@@ -96,7 +96,7 @@ inline namespace v1 {
       m_vertices.append(vertices[1]);
     }
 
-    for (int j = 0; j < m_gridSize.height; ++j) {
+    for (int j = 0; j <= m_gridSize.height; ++j) {
       float y = j * m_cellSize.height;
       vertices[0].position = { 0.0f, y };
       vertices[1].position = { max.x, y };
